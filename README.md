a9ae2fadbc4a0938af387e3d85cf51d5dcd3264f
Kuan Tung <b03901039@ntu.edu.tw>
2017-06-25 15:57:10 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+Swip Hockey
+=========
+
+#### Setup
+
+  yarn global add webpack
+  yarn install
+  cd games/hockey
+  yarn install
+  cd ../..
+  yarn run build
+
+#### Run server
+
+  yarn run hockey
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  entry: [
+    './src/client/index.js',
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader', // 'babel-loader' is also a legal name to reference
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader',
+      },
+    ],
+  },
+};
